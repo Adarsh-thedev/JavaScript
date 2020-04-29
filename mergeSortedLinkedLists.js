03e4454aef6a5aed86718116e8800095267df3d6
Adarsh-thedev <adarshpittu@gmail.com>
2020-04-29 14:41:21 +0530

    merge two sorted linked lists

--- mergeSortedLinkedLists.js
+++ mergeSortedLinkedLists.js
@@ -46,6 +46,7 @@ myList2.append(20);
 console.log(mergeSortedList(myList2, myList1));
 
 function mergeSortedList(list1, list2) {
+    //If either of the LinkedLists are empty, return the another LinkedList
     if(list1.length === 0) {
         return list2;
     }
@@ -54,16 +55,25 @@ function mergeSortedList(list1, list2) {
         return list1;
     }
 
+    //Initiate two variable containig heads of both LinkedLists
     let listOneItem = list1.head, listTwoItem = list2.head;
     let newList = new LinkedList();
     while(listOneItem && listTwoItem) {
+        //If value of listOneItem is less then push it inside the linked list 
+        //and pass the reference of listOneItem to the next item in the first LinkedList 
         if(listOneItem.value < listTwoItem.value) {
             newList.append(listOneItem);
             listOneItem = listOneItem.next;
-        } else {
+        }
+        //Else do the same thing with second LinkedList
+        else {
             newList.append(listTwoItem);
             listTwoItem = listTwoItem.next;
         }
+
+        //If any of the next pointers reffers to null, i.e end of the linked list
+        //pass the reference to the another LinkedList 
+        //and add rest of the nodes into newList as the previous list was already sorted
         if(listOneItem === null) {
             newList.append(listTwoItem);
         }
