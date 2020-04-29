a57c57197f8154e5a25e47c25246225b079a7bde
Adarsh-thedev <adarshpittu@gmail.com>
2020-04-29 14:27:41 +0530

    merge two sorted linked lists

---
+++ mergeSortedLinkedLists.js
@@ -0,0 +1,75 @@
+//Merge two sorted linked lists and return it as a new list. 
+//The new list should be made by splicing together the nodes of the first two lists.
+
+class LinkedList {
+    constructor() {
+        this.head = {
+            value : null,
+            next : null
+        }
+        this.tail = {
+            value : null,
+            next : null
+        }
+        this.length = 0;
+    }
+
+    append(value) {
+        let newNode = {
+            value : value,
+            next : null
+        }
+        if(this.head.value === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+const myList1 = new LinkedList();
+myList1.append(10);
+myList1.append(11);
+myList1.append(15);
+myList1.append(19);
+
+const myList2 = new LinkedList();
+myList2.append(12);
+myList2.append(13);
+myList2.append(14);
+myList2.append(17);
+myList2.append(20);
+console.log(mergeSortedList(myList2, myList1));
+
+function mergeSortedList(list1, list2) {
+    if(list1.length === 0) {
+        return list2;
+    }
+    
+    if(list2.length === 0) {
+        return list1;
+    }
+
+    let listOneItem = list1.head, listTwoItem = list2.head;
+    let newList = new LinkedList();
+    while(listOneItem && listTwoItem) {
+        if(listOneItem.value < listTwoItem.value) {
+            newList.append(listOneItem);
+            listOneItem = listOneItem.next;
+        } else {
+            newList.append(listTwoItem);
+            listTwoItem = listTwoItem.next;
+        }
+        if(listOneItem === null) {
+            newList.append(listTwoItem);
+        }
+        if(listTwoItem === null) {
+            newList.append(listOneItem);
+        }
+    }
+    return newList;
+}
