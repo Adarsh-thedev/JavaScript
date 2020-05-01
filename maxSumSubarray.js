0e2e526e37375424e43ccb6cd0281654b5573fcd
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-01 09:15:54 +0530

    max sum subarray

--- maxSumSubarray.js
+++ maxSumSubarray.js
@@ -4,16 +4,29 @@
 
 const maxSubarray = (array) => {
     let maxSoFar = array[0], maxEndingHere = 0;
+    let start = 0, end = 0, s = 0;
+
     for(let i = 0; i < array.length; i++) {
         maxEndingHere = maxEndingHere + array[i];
+
         if(maxEndingHere > maxSoFar) {
             maxSoFar = maxEndingHere;
+            start = s;
+            end = i;
         }
+
         if(maxEndingHere < 0) {
             maxEndingHere = 0;
+            s = i + 1;
         }
     }
-    return maxSoFar;
+
+    let maxSumSubarray = [];
+    for(let i = start; i <= end; i++) {
+        maxSumSubarray.push(array[i]);
+    }
+    console.log(maxSoFar);
+    return maxSumSubarray;
 }
 
 console.log(maxSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
