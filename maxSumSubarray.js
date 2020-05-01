9681c0eb17da22da8512ad650d421b5fdf71d062
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-01 09:26:31 +0530

    max sum subarray

--- maxSumSubarray.js
+++ maxSumSubarray.js
@@ -6,15 +6,19 @@ const maxSubarray = (array) => {
     let maxSoFar = array[0], maxEndingHere = 0;
     let start = 0, end = 0, s = 0;
 
+    //Itrate through the array and kepp track of sum till the iterated index 
     for(let i = 0; i < array.length; i++) {
         maxEndingHere = maxEndingHere + array[i];
 
+        //If sumTill the iterated index is greater than maxSoFar i.e new max is found
+        //set maxSoFar to maxEndingHere
         if(maxEndingHere > maxSoFar) {
             maxSoFar = maxEndingHere;
             start = s;
             end = i;
         }
 
+        //If maxEndingHere is negative set it to 0 to start finding new maxSum
         if(maxEndingHere < 0) {
             maxEndingHere = 0;
             s = i + 1;
